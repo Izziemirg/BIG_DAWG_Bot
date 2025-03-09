3f718b649173efdbd34e7201278d79f944c35eda
Izziemirg <Mirghani42@gmail.com>
2025-03-09 19:16:46 -0400

    Updated and personalized responses.py

--- responses.py
+++ responses.py
@@ -15,33 +15,66 @@ user_responses = {
         "GMU wants that M.S. back",
         "Little leprechaun",
         "Gonna take a scroll through Jose's old Facebook photos",
-        "How are your Farsi lessons going?"
+        "How are your Farsi lessons going?",
+        "BIGGG hot tub guy, huh?"
     ],
     "izzie": [
         "Not sure why yall don’t pick up Izzie"
     ],
     "cheggstudy": [
         "You still using Chegg in 2025?",
-        "CheggStudy out here doing your homework again?"
+        "Ruben Amorim your lord and savior, huh?",
+        "Your lego collection is mid at best",
+        "The Harry Maguire of gaming"
     ],
     "easybake8992": [
         "Syracuse wants their M.S. back",
-        "Cookin up another L?"
+        "Cookin up another L?",
+        "The Eazybake oven only cookin baby food!",
+        "Sneaky athletic",
+        "Can we kick him out the chat?",
+        "Syd, please take his electronics away",
+        "Ipad kid vibes",
+        "Jose is your indentured servant",
+        "Don't make me drive to Delaware rn",
+        "Does Syd make you dress up like pulp fiction?",
+        "I heard you peed in a closet",
+        "I'm more of an Milwaukee guy than a Dewalt guy....",
+        "Best data scientist I have ever met tbh",
+        "Who did you steal your snowboard from?",
+        "I wish Evan could grapple with reality"
+        "White, no tats"
     ],
     "karim.zebdi": [
-        "Karim, you dropping hot or hiding?",
-        "Zebdi out here acting like he’s got a 5.0 K/D."
+        "Its MR. ZEBDI to you",
+        "Karim why don't you share the old vaccums man?",
+        "Khalil is my favorite Zebdi",
+        "Venmo request me $0.67 man",
+        "Everytime he says something important, his nose grows!",
+        "HES HUGE!",
+        "Best bladder to have on a roadtrip",
+        "VERY Algerian of you man",
+        "That is pretty Algerian man",
+        "Someone give him the boot",
+        "El fado?",
+        "Used car salesman vibes",
+        "All I know is NASA is cooked if you work for them",
+        "The Nicholas Pepe of gaming",
+        "Sand monkey vibes"
     ],
     "noonxo": [
         "You like Egyptians too much for me to trust you",
-        "Very Indian of you",
+        "Very Indian of you man",
         "FOR PAKISTANNNNN"
     ],
     "zsheisty2498": [
         "You ain't a thug",
         "Undershirt killa",
         "Raw dawgin life huh?",
-        "You movin booky"
+        "You movin booky",
+        "Most productive worker the US government has",
+        "Golf ball bully",
+        "Athletic af for no reason"
     ],
     "samie_mirghani": [
         "Samie out here thinking he’s the main character!",
